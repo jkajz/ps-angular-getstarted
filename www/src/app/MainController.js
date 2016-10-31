8f62501bdbe5e29b90955a687dd3e24897b4fe71
Jani <jani.kajzovar@gmail.com>
2016-10-31 20:34:26 +0100

    Finished Directives and Views

--- www/src/app/MainController.js
+++ www/src/app/MainController.js
@@ -2,16 +2,28 @@
 
 module.exports = function($scope, $http) {
 
+    var onRepos = function(response) {
+        $scope.repos = response.data;
+    };
+
     var onUserComplete = function(response) {
         $scope.user = response.data;
+        $http.get($scope.user.repos_url)
+             .then(onRepos, onError);
     };
 
+    
+        
     var onError = function(error) {
-        $scope.error = 'Could not fetch user';
+        $scope.error = 'Could not fetch data';
+    };
+    
+    $scope.search = function(username) {
+        $http.get('https://api.github.com/users/' + username)
+        .then(onUserComplete, onError);
     };
 
-    $http.get('https://api.github.com/users/robconery')
-        .then(onUserComplete, onError);
-
-    $scope.message = 'Hello Angular';
+    $scope.username = 'angular';
+    $scope.message = 'Github Viewer';
+    $scope.repoSortOrder = '-stargazers_count';
 };
